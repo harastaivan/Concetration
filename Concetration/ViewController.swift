a07fb97671845e2a5bf5cebf72c2cb7d30ddc797
Ivan Harasta <harasta.ivan@gmail.com>
2019-02-15 10:57:45 +0100

    Initial Commit

---
+++ Concetration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concetration
+//
+//  Created by Ivan Harašta on 15/02/2019.
+//  Copyright © 2019 Ivan Harašta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

996bc2000dca20990e249f52a66ac46cb83cde75
Ivan Harasta <harasta.ivan@gmail.com>
2019-02-15 14:17:30 +0100

    Add 2 cards and flipCount and flip functionality

--- Concetration/ViewController.swift
+++ Concetration/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount =  0 // same as var flipCount: Int =  0
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCountLabel.text = "Flips: \(flipCount)"
+        flipCard(withEmoji: "😂", on: sender)
     }
 
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCountLabel.text = "Flips: \(flipCount)"
+        flipCard(withEmoji: "😡", on: sender)
+    }
 
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 

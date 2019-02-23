50ff601529277c165a2e689cb80ace69126bac19
Ivan Harasta <harasta.ivan@gmail.com>
2019-02-23 23:41:54 +0100

    Compiler automatically generates this code
    https://useyourloaf.com/blog/how-to-get-equatable-and-hashable-for-free/

--- Concetration/Card.swift
+++ Concetration/Card.swift
@@ -10,14 +10,6 @@ import Foundation
 
 struct Card: Hashable {
 
-    var hashValue: Int {
-        return identifier
-    }
-
-    static func ==(lhs: Card, rhs: Card) -> Bool {
-        return lhs.identifier == rhs.identifier
-    }
-
     var isFaceUp = false
     var isMatched = false
     private var identifier: Int

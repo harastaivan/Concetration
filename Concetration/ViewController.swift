0ab16ef1bd9bd076aa8d35e359bda7976ad258db
Ivan Harasta <harasta.ivan@gmail.com>
2019-02-23 23:31:59 +0100

    Change emojiChoices from Array to String

--- Concetration/ViewController.swift
+++ Concetration/ViewController.swift
@@ -48,13 +48,15 @@ class ViewController: UIViewController {
         }
     }
 
-    private var emojiChoices = ["😂", "😡", "😠", "😅", "😍", "🤮", "😎", "😀", "😮", "😻", "😹", "🤔", "👀"]
+    // private var emojiChoices = ["😂", "😡", "😠", "😅", "😍", "🤮", "😎", "😀", "😮", "😻", "😹", "🤔", "👀"]
+    private var emojiChoices = "😂😡😠😅😍🤮😎😀😮😻😹🤔👀"
 
     private var emoji = [Card:String]()
 
     private func emoji(for card: Card) -> String {
         if emoji[card] == nil, emojiChoices.count > 0 {
-            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
         return emoji[card] ?? "?"
     }
